d5cb5aec8e26337e9d4f9168e2c42e0d1bfdcf37
Cameron Hetzler <cjhetz@gmail.com>
2023-10-18 16:15:23 -0400

    Init

---
+++ Program.cs
@@ -0,0 +1,149 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using System.Collections.Concurrent;
+using System.Runtime.CompilerServices;
+/**
+static async void Main(string[] args) {
+    // read files
+    String fileName1 = args[0], fileName2 = args[1], fileName3 = args[2];
+    Task<List<String>> readFile1 = AsyncReadFileWords(fileName1);
+    Task<List<String>> readFile2 = AsyncReadFileWords(fileName2);
+    Task<List<String>> readFile3 = AsyncReadFileWords(fileName3);
+
+    await Task.WhenAll(readFile1, readFile2, readFile3);
+
+    List<String> words1 = await readFile1;
+    List<String> words2 = await readFile2;
+    List<String> words3 = await readFile3;
+
+    SortWords(words1 , words2, words3);
+}
+
+static List<String> SortWords(List<String> words1, List<String> words2, List<String> words3)
+{
+    List<String> newList = new List<String>();
+    List<String> _tempList = new List<String>();
+
+    for (int i  = 0; i < words1.Count && i < words2.Count && i < words3.Count; ++i)
+    {
+        // create local variable for all words in INDEX 0
+        // each iteration we compare the 3 local variables and then
+        // delete them from each list.
+        _tempList.Add(words1[0]);
+        _tempList.Add(words2[0]);
+        _tempList.Add(words3[0]);
+
+        // TODO: Check if this is actually ASC
+        _tempList.Sort();
+
+        // because we iterate only through the length of the shortest,
+        // we want to add all three words each iteration
+
+        foreach (String word in _tempList) {
+            if (newList.Count > 0 &&
+                !newList[newList.Count-1].Equals(word))
+                newList.Add(word);
+        }
+
+        //Now delete each of the words from the total lists
+        words1.RemoveAt(0);
+        words2.RemoveAt(0);
+        words3.RemoveAt(0);
+
+        _tempList = new List<string>();
+    }
+
+    // Now we need to worry about which lists still contain content.
+    // there's def a better way than hard coding this but that's for later
+
+
+    return null;
+}
+*/
+
+
+
+
+class ReadingFiles
+{
+    static void Main(String[] args)
+    {
+        Console.WriteLine(args[0]);
+        Console.ReadLine();
+
+        ConcurrentDictionary<String, int> words = new ConcurrentDictionary<string, int>();
+        String fileName1 = args[0], fileName2 = args[1], fileName3 = args[2];
+        Task readFile1 = Task.Run(() => AsyncReadFileWords(fileName1, words));
+        Task readFile2 = Task.Run(() => AsyncReadFileWords(fileName2, words));
+        Task readFile3 = Task.Run(() => AsyncReadFileWords(fileName3, words));
+
+        Task.WaitAll(readFile1, readFile2, readFile3);
+
+        Dictionary<String, int> wordsOrdered;
+        wordsOrdered = words.OrderBy(x => x.Key.ToLower()).ToDictionary(x => x.Key, x => x.Value);
+
+
+        foreach (String key in wordsOrdered.Keys)
+        {
+            Console.Write(key  + '\n');
+        }
+
+        WriteWordsToFile("file4.txt", wordsOrdered.Keys.ToList());
+
+        Console.ReadLine();
+    }
+
+    static void WriteWordsToFile(String filename, List<String> wordList)
+    {
+        using (StreamWriter sw = new StreamWriter(filename)) {
+            foreach (String word in wordList)
+            {
+                sw.WriteLine(word);
+            }
+        }
+    }
+
+    static void AsyncReadFileWords(String filename, ConcurrentDictionary<String, int> words)
+    {
+        if (!File.Exists(filename))
+            return;
+
+        using (StreamReader sr = new StreamReader(filename))
+        {
+            // want to read not line by line but word by word
+            // sep by "space" for now
+            while (!sr.EndOfStream)
+            {
+                String word = GetNextWord(sr, ' ');
+
+                if (word != String.Empty)
+                {
+                    words.AddOrUpdate(word, 1, (s, i) => i + 1);
+                }
+
+            }
+        }
+    }
+
+    static String GetNextWord(StreamReader sr, Char delim)
+    {
+        String word = "";
+
+        char c;
+        do
+        {
+            c = Convert.ToChar(sr.Read());
+
+            // is the character an alpha character
+            if ((c >= 65 && c <= 90) ||
+                (c >= 97 && c <= 122))
+                word += c;
+        } while (!sr.EndOfStream && c != delim);
+        return word;
+
+    }
+}
+
+
+
